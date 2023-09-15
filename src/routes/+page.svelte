65d80c20b9c9b9f93a5e76839b28bd8d09d87116
Kip Dunlap <kipling.dunlap@gmail.com>
2023-09-15 11:52:46 -0400

    table'ify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,19 +7,40 @@
 <h1>NPU Chairs Appreciation Dinner</h1>
 <a href="/RSVP">RSVP</a> | <a href="/checkin">Check In</a>
 <p>RSVPs go here:</p>
-{#each RSVPs as RSVP}
-  <div>
-    <p>NPU: {RSVP.NPU}</p>
-    <p>ATTENDEE: {RSVP.FNAME} {RSVP.LNAME}</p>
-    <p>GUEST: {RSVP.GUEST || ''}</p>
-    <p>DIET: {RSVP.DIET || ''}</p>
-    <p>RSVP'd: {RSVP.RSVPd ? '✅' : '❌'}</p>
-    <p>ATTENDED: {RSVP.ATTENDED ? '✅' : '❌'}</p>
-    <form action="?/delete" method="POST">
-      <button>DELETE</button>
-    </form>
-  </div>
-{/each}
+<table width="100%" border="1px solid black">
+  <thead>
+    <tr>
+      <th>NPU</th>
+      <th>ATTENDEE</th>
+      <th>GUEST</th>
+      <th>DIET</th>
+      <th>RSVP'd</th>
+      <th>ATTENDED</th>
+      <th>EDIT</th>
+      <th>DELETE</th>
+    </tr>
+  </thead>
+  {#each RSVPs as RSVP}
+    <tr>
+      <td><strong>{RSVP.NPU}</strong></td>
+      <td>{RSVP.FNAME} {RSVP.LNAME}</td>
+      <td>{RSVP.GUEST || '-'}</td>
+      <td>{RSVP.DIET || '-'}</td>
+      <td>{RSVP.RSVPd ? '✅' : '❌'}</td>
+      <td>{RSVP.ATTENDED ? '✅' : '❌'}</td>
+      <td>
+        <form action="?/edit" method="POST">
+          <button>EDIT</button>
+        </form>
+      </td>
+      <td>
+        <form action="?/delete" method="POST">
+          <button>DELETE</button>
+        </form></td
+      >
+    </tr>
+  {/each}
+</table>
 
 <style>
   p {
@@ -27,10 +48,10 @@
     margin: 0;
   }
 
-  div {
-    margin: 1svw;
+  td {
+    padding: 0.1svw;
   }
-  div:nth-child(even) {
-    background-color: whitesmoke;
+  tr:nth-child(even) {
+    background-color: rgb(236, 236, 236);
   }
 </style>
