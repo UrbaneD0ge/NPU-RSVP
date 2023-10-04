369470a938fb33581782bee07c9775f355cd14ef
Kip <Kipling.Dunlap@gmail.com>
2023-10-04 13:43:01 -0400

    invite is rsvp link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
-<img
-  src="STD.png"
-  alt="City of Atlanta | Department of City Planning, Neighborhood Planning Units. Save the Date for the Annual NPU Chairs' Appreciation Dinner: Thursday, November 9, 2023"
-/>
+<a href="/RSVP"
+  ><img
+    src="STD.png"
+    alt="City of Atlanta | Department of City Planning, Neighborhood Planning Units. Save the Date for the Annual NPU Chairs' Appreciation Dinner: Thursday, November 9, 2023"
+  /></a
+>
 
 <style>
   img {
